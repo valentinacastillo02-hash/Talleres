aef9d2aa8bd5eec8c759ef97a671bc8a920e7208
valentinacastillo02-hash <valentina.castillo02@alumnos.ucn.cl>
2025-10-11 23:48:36 -0300

    Update Usuarioscodigo.java

--- TallerDos/src/Dominio/Usuarioscodigo.java
+++ TallerDos/src/Dominio/Usuarioscodigo.java
@@ -1,42 +1,33 @@
+//Emiliano Angel Toro Rojas, RUT: 21.512.702-8, Carrera: Ingenieria en tecnologias de información.
+//Valentina Castillo,Rut; 15.166.692-2, Carrera: Ingenieria en tecnologias de información.
+
+
 package Dominio;
 
-
 public class Usuarioscodigo {
-	private String Username;
-	private String contrasena;
-	private String rol;
-	
-	public Usuarioscodigo(String Username,String contrasena,String rol) {
-		this.setUsername(Username);
-		this.setContrasena(contrasena);
-		this.setRol(rol);
-		
-	}
+    private String username;
+    private String contrasenaHash;
+    private String rol;
 
-	public String getUsername() {
-		return Username;
-	}
+    public Usuarioscodigo(String username, String contrasenaHash, String rol) {
+        this.username = username;
+        this.contrasenaHash = contrasenaHash;
+        this.rol = rol;
+    }
 
-	public void setUsername(String username) {
-		Username = username;
-	}
-
-	public String getContrasena() {
-		return contrasena;
-	}
-
-	public void setContrasena(String contrasena) {
-		this.contrasena = contrasena;
-	}
-
-	public String getRol() {
-		return rol;
-	}
-
-	public void setRol(String rol) {
-		this.rol = rol;
-	}
-	
-	
-	
+    public String getUsername() { 
+    	return username; 
+    	}
+    public String getContrasenaHash() { 
+    	return contrasenaHash; 
+    	}
+    public String getRol() { 
+    	return rol; 
+    	}
+    public boolean esAdmin() { 
+    	return rol.equalsIgnoreCase("ADMIN"); 
+    	}
+    public boolean esUser() { 
+    	return rol.equalsIgnoreCase("USER"); 
+    	}
 }
